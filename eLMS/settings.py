4e2d4545a6101b1e3d0cd187baa934704703d56c
miguel <miguellaravilla8@gmail.com>
2024-06-23 22:53:06 -0500

    upgrate for deployment

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,8 +158,10 @@ USE_TZ = True
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 STATIC_URL = '/static/'
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
